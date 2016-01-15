8e3ac84e6bc558f7186ef422a5b7d432b34cd698
John Wheal <john@whealmedia.com>
2016-01-15 16:33:21 +0000

    Add opportunity custom post type

--- functions.php
+++ functions.php
@@ -209,3 +209,57 @@ function member_post_type() {
 	register_post_type( 'member', $args );
 
 }
+
+
+/**
+* Register opportunites custom post type
+*/
+add_action('init', 'opportunity_post_type', 0);
+function opportunity_post_type() {
+
+	$labels = array(
+		'name'                => _x( 'Opportunity', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Opportunity', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Opportunities', 'text_domain' ),
+		'parent_item_colon'   => __( '', 'text_domain' ),
+		'all_items'           => __( 'All Opportunities', 'text_domain' ),
+		'view_item'           => __( 'View Opportunity', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Opportunity', 'text_domain' ),
+		'add_new'             => __( 'New Opportunity', 'text_domain' ),
+		'edit_item'           => __( 'Edit Opportunity', 'text_domain' ),
+		'update_item'         => __( 'Update Opportunity', 'text_domain' ),
+		'search_items'        => __( 'Search Opportunities', 'text_domain' ),
+		'not_found'           => __( 'No opportunities found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'No opportunities found in bin', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                => 'opportunity',
+		'with_front'          => true,
+		'pages'               => false,
+		'feeds'               => false,
+	);
+	$args = array(
+		'label'               => __( 'member', 'text_domain' ),
+		'description'         => __( 'nest members', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor'),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => false,
+		'menu_position'       => 20,
+		'menu_icon'           => 'dashicons-smiley',
+		'can_export'          => true,
+		'has_archive'         => false,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'query_var'           => 'opportunity',
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'opportunity', $args );
+
+}
+
