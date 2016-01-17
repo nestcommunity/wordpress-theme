194feb5e1b726c0aeb27c7c17c68563242eb512a
Zac Colley <hi@zaccolley.com>
2016-01-17 21:48:29 +0000

    Added a quick idea for listing people

--- htmlHeader.php
+++ htmlHeader.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link href='https://fonts.googleapis.com/css?family=Old+Standard+TT:400,400italic,700' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Muli:400,300,300italic,400italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,900italic,900,700italic,700,500italic,500,400italic,300italic,300,100italic,100' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/styles.css" />
 
   <title>nest</title>
--- main.php
+++ main.php
@@ -12,4 +12,74 @@
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </section>
   </div>
+  <div class="row">
+    <div class="medium-7 medium-offset-1 columns">
+      <ul class="people">
+        <li class="person">
+          <img src="http://fillmurray.com/100/100" alt="" />
+          <span>Bill Murray</span>
+        </li>
+        <li class="person">
+          <img src="http://placecage.com/100/100" alt="" />
+          <span>Nic Cage</span>
+        </li>
+        <li class="person">
+          <img src="http://stevensegallery.com/100/100" alt="" />
+          <span>Steven Segal</span>
+        </li>
+        <li class="person">
+          <img src="http://lorempizza.com/100/100" alt="" />
+          <span>Pizza</span>
+        </li>
+        <li class="person">
+          <img src="http://fillmurray.com/100/100" alt="" />
+          <span>Bill Murray</span>
+        </li>
+        <li class="person">
+          <img src="http://placecage.com/100/100" alt="" />
+          <span>Nic Cage</span>
+        </li>
+        <li class="person">
+          <img src="http://stevensegallery.com/100/100" alt="" />
+          <span>Steven Segal</span>
+        </li>
+        <li class="person">
+          <img src="http://lorempizza.com/100/100" alt="" />
+          <span>Pizza</span>
+        </li>
+        <li class="person">
+          <img src="http://fillmurray.com/100/100" alt="" />
+          <span>Bill Murray</span>
+        </li>
+        <li class="person">
+          <img src="http://placecage.com/100/100" alt="" />
+          <span>Nic Cage</span>
+        </li>
+        <li class="person">
+          <img src="http://stevensegallery.com/100/100" alt="" />
+          <span>Steven Segal</span>
+        </li>
+        <li class="person">
+          <img src="http://lorempizza.com/100/100" alt="" />
+          <span>Pizza</span>
+        </li>
+        <li class="person">
+          <img src="http://fillmurray.com/100/100" alt="" />
+          <span>Bill Murray</span>
+        </li>
+        <li class="person">
+          <img src="http://placecage.com/100/100" alt="" />
+          <span>Nic Cage</span>
+        </li>
+        <li class="person">
+          <img src="http://stevensegallery.com/100/100" alt="" />
+          <span>Steven Segal</span>
+        </li>
+        <li class="person">
+          <img src="http://lorempizza.com/100/100" alt="" />
+          <span>Pizza</span>
+        </li>
+      </ul>
+    </div>
+  </div>
 </main>
