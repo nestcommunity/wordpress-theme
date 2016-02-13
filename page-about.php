fe5cd45815cba788ae7ee62bd0c78398b52d2eb7
John Wheal <john@whealmedia.com>
2016-02-13 23:15:30 +0000

    Added home and about page sub-headings

--- page-about.php
+++ page-about.php
@@ -9,11 +9,10 @@ get_header(); ?>
 		<div class="page-background about-background"></div>
 		<div class="row about contents">
 			<?php while ( have_rows('sections') ) : the_row(); ?>
-
-				<div class="columns medium-6">
+				<div class="columns medium-6 charlottely-headings">
 					<h3><?php the_sub_field('heading'); ?></h3>
 				</div>
-				<div class="columns medium-6">
+				<div class="columns medium-6 body-text">
 					<p><?php the_sub_field('contents'); ?></p>
 				</div>
 			<?php endwhile; ?>
--- page-home.php
+++ page-home.php
@@ -8,12 +8,12 @@ get_header(); ?>
 	<main class="home">
 		<div class="page-background home-background"></div>
 		<div class="row about contents">
-			<div class="columns medium-6">
+			<div class="columns medium-6 charlottely-headings">
 				<h2>Turning ideas<br/>into action</h2>
 			</div>
-			<div class="columns medium-6">
+			<div class="columns medium-6 body-text">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<p><?php the_content(); ?></p>
+					<?php the_content(); ?>
 				<?php endwhile; endif; ?>
 			</div>
 		</div>
