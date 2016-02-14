35dd9cb696212c12547f925702e494ca7521708c
John Wheal <john@whealmedia.com>
2016-02-14 20:38:56 +0000

    Added generic page template

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+	<main class="members general-page">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<div class="heading">
+				<div class="row">
+					<div class="columns medium-12">
+						<h1><?php the_title(); ?></h1>
+					</div>
+				</div>
+			</div>
+			<div class="contents body-text">
+				<div class="row">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		<?php endwhile; endif; ?>
+	</main>
+
+<?php get_footer(); ?>
