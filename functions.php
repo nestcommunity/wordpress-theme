3148689e5c7355bb6f4f05f7f482509ad73c025b
John Wheal <john@whealmedia.com>
2016-02-29 10:15:25 +0000

    Removed programmes custom post type

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
 */
 
 //Remove ACF
-define('ACF_LITE', false);
+define('ACF_LITE', true);
 
 //Include plugins
 //require_once('plugins/custom-permalinks/custom-permalinks.php');
@@ -385,56 +385,3 @@ function resource_category_post_type() {
 	register_post_type( 'categories', $args );
 
 }
-
-/**
-* Register programmes custom post type
-*/
-add_action('init', 'programme_post_type', 0);
-function programme_post_type() {
-
-	$labels = array(
-		'name'                => _x( 'Programme', 'Post Type General Name', 'text_domain' ),
-		'singular_name'       => _x( 'Programme', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'           => __( 'Programmes', 'text_domain' ),
-		'parent_item_colon'   => __( '', 'text_domain' ),
-		'all_items'           => __( 'All Programmes', 'text_domain' ),
-		'view_item'           => __( 'View Programme', 'text_domain' ),
-		'add_new_item'        => __( 'Add New Programme', 'text_domain' ),
-		'add_new'             => __( 'New Programme', 'text_domain' ),
-		'edit_item'           => __( 'Edit Programme', 'text_domain' ),
-		'update_item'         => __( 'Update Programme', 'text_domain' ),
-		'search_items'        => __( 'Search Programmes', 'text_domain' ),
-		'not_found'           => __( 'No programmes found', 'text_domain' ),
-		'not_found_in_trash'  => __( 'No programmes found in bin', 'text_domain' ),
-	);
-	$rewrite = array(
-		'slug'                => 'programme',
-		'with_front'          => true,
-		'pages'               => false,
-		'feeds'               => false,
-	);
-	$args = array(
-		'label'               => __( 'programme', 'text_domain' ),
-		'description'         => __( 'nest programme', 'text_domain' ),
-		'labels'              => $labels,
-		'supports'            => array( 'title', 'editor'),
-		'hierarchical'        => false,
-		'public'              => true,
-		'show_ui'             => true,
-		'show_in_menu'        => true,
-		'show_in_nav_menus'   => true,
-		'show_in_admin_bar'   => false,
-		'menu_position'       => 20,
-		'menu_icon'           => 'dashicons-welcome-learn-more',
-		'can_export'          => true,
-		'has_archive'         => false,
-		'exclude_from_search' => false,
-		'publicly_queryable'  => true,
-		'query_var'           => 'programme',
-		'rewrite'             => $rewrite,
-		'capability_type'     => 'page',
-	);
-	register_post_type( 'programme', $args );
-
-}
-
