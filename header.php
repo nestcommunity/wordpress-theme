4a33a4553015b04a264ec6f5f381e99e6b4ed962
John Wheal <john@whealmedia.com>
2016-03-21 12:39:41 +0000

    Added new background

--- header.php
+++ header.php
@@ -1,16 +1,10 @@
 <?php require_once('htmlHeader.php') ?>
 <div class="header">
-  <div class="row">
-    <div class="medium-2 columns">
-      <a href="/"><h1 class="logo logo--black">nest</h1></a>
-    </div>
-    <div class="medium-10 columns">
-      <?php wp_nav_menu( array(
-            'theme_location' => 'top-menu',
-            'container' => false,
-            'items_wrap' => '<ul class="menu">%3$s</ul>',
-          )
-      ); ?>
-    </div>
-  </div>
+    <a href="/"><h1 class="logo logo--black">nest</h1></a>
+    <?php wp_nav_menu( array(
+          'theme_location' => 'top-menu',
+          'container' => false,
+          'items_wrap' => '<ul class="menu">%3$s</ul>',
+        )
+    ); ?>
 </div>
